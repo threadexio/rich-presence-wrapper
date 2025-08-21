3d9fdee956a97f8ad86728988debf17ee8082aaa
threadexio <pzarganitis@gmail.com>
2025-08-21 14:23:42 +0300

    preserve other files of the wrapper package

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -11,44 +10,135 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs =
+    { self
+    , nixpkgs
+    , rust-overlay
+    , ...
+    }:
     let
-      mkPkgs = system: import nixpkgs {
-        inherit system;
-        overlays = [
-          (import rust-overlay)
+      systems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
 
-          (final: _: {
-            rustToolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          })
+      inherit (nixpkgs) lib;
 
-          (final: _: {
-            rich-presence-wrapper = final.callPackage ./nix/package.nix { };
-          })
-        ];
-      };
+      mkPkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+
+            (final: _: {
+              rustToolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+            })
+
+            (final: _: {
+              rich-presence-wrapper = final.callPackage ./nix/package.nix { };
+            })
+          ];
+        };
+
+      perSystem = f: lib.genAttrs systems (system: f (mkPkgs system));
     in
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = mkPkgs system;
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
+    {
+      formatter = perSystem (pkgs: pkgs.nixpkgs-fmt);
 
-        packages.default = pkgs.rich-presence-wrapper;
-
-        devShells.default = pkgs.mkShell {
+      devShells = perSystem (pkgs: {
+        default = pkgs.mkShell {
           packages = with pkgs; [
             rustToolchain
             helix
           ];
 
-          env._hx = "${pkgs.helix}/bin/hx";
+          env._hx = lib.getExe pkgs.helix;
         };
+      });
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = pkgs.rich-presence-wrapper;
+      packages = perSystem (pkgs: {
+        default = pkgs.rich-presence-wrapper;
+      });
+
+      apps = perSystem (pkgs: {
+        default = {
+          type = "app";
+          program = lib.getExe pkgs.rich-presence-wrapper;
         };
-      }
-    );
+      });
+
+      overlays =
+        let
+          mkOverlay =
+            { package
+            , pathsToLink ? [ ]
+            , wrapperArgs ? [ ]
+            , ...
+            }:
+            final: prev:
+            let
+              drv = prev.${package};
+              programName = drv.meta.mainProgram or drv.pname;
+
+              rich-presence-wrapper =
+                lib.throwIfNot (lib.hasAttr "rich-presence-wrapper" final)
+                  "The '${package}' overlay of `rich-presence-wrapper` requires that the package `rich-presence-wrapper` is available in `pkgs`. Did you forget to include the 'default' overlay?"
+                  final.rich-presence-wrapper;
+            in
+            {
+              "${package}" = final.buildEnv {
+                name = "${drv.pname}-rich-presence-wrapper";
+
+                paths = [
+                  rich-presence-wrapper
+                  drv
+                ];
+
+                inherit pathsToLink;
+
+                nativeBuildInputs = [ final.makeWrapper ];
+
+                postBuild = ''
+                  mkdir -p $out/bin
+
+                  makeWrapper ${lib.getExe rich-presence-wrapper} $out/bin/${programName} \
+                    --set _${programName} ${lib.getExe drv} \
+                    --inherit-argv0 \
+                    ${lib.concatStringsSep " " wrapperArgs}
+                '';
+              };
+            };
+        in
+        {
+          default = final: _: {
+            rich-presence-wrapper = self.packages.${final.system}.default;
+          };
+
+          helix = mkOverlay {
+            package = "helix";
+
+            pathsToLink = [
+              "/share"
+              "/lib"
+            ];
+          };
+
+          zed-editor = mkOverlay {
+            package = "zed-editor";
+
+            pathsToLink = [
+              "/share"
+              "/libexec"
+            ];
+
+            wrapperArgs = [
+              "--add-flags"
+              "--foreground"
+            ];
+          };
+        };
+    };
 }
--- nix/package.nix
+++ nix/package.nix
@@ -1,54 +1,36 @@
 { rustToolchain
 , makeRustPlatform
-, makeWrapper
-, helix
-, zed-editor
 , lib
-, programs ? [ "helix" "zed" ]
 , ...
 }:
-with builtins;
 
 let
-  manifest = fromTOML (readFile ../Cargo.toml);
+  manifest = lib.importTOML ../Cargo.toml;
+
   rustPlatform = makeRustPlatform {
     cargo = rustToolchain;
     rustc = rustToolchain;
   };
-
-  availablePrograms = {
-    "helix" = { program = "${helix}/bin/hx"; package = helix; };
-    "zed" = { program = "${zed-editor}/bin/zeditor"; package = zed-editor; wrapperArgs = "--add-flags --foreground"; };
-  };
-
-  mapEnabledPrograms = f: map (name: f name availablePrograms.${name}) programs;
-
-  pname = manifest.package.name;
-  version = manifest.package.version;
 in
+
 rustPlatform.buildRustPackage {
-  inherit pname version;
-  src = ../.;
+  pname = manifest.package.name;
+  inherit (manifest.package) version;
+
+  src =
+    with lib.fileset;
+    toSource {
+      root = ../.;
+      fileset = unions [
+        ../src
+        ../Cargo.toml
+        ../Cargo.lock
+      ];
+    };
 
   cargoLock.lockFile = ../Cargo.lock;
 
-  inherit programs;
-  availablePrograms = attrNames availablePrograms;
-
   doCheck = false;
-  buildNoDefaultFeatures = true;
-  buildFeatures = mapEnabledPrograms (name: _: name);
-  buildInputs = mapEnabledPrograms (_: { package, ... }: package);
-  nativeBuildInputs = [ makeWrapper ];
-
-  postInstall =
-    let
-      installCmd = { program, wrapperArgs ? "", ... }:
-        "makeWrapper $out/bin/${pname} $out/bin/${baseNameOf program} --inherit-argv0 --set _${baseNameOf program} ${program} ${wrapperArgs}";
-
-      installCmds = mapEnabledPrograms (_: installCmd);
-    in
-    lib.concatLines installCmds;
 
   meta = with lib; {
     description = "Discord rich presence wrapper";
