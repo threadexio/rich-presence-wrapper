3612550e8e77447ac70280c4f9a6ce797927d6b8
threadexio <pzarganitis@gmail.com>
2026-04-30 10:08:14 +0300

    add template

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A very basic flake.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      inherit (nixpkgs) lib;
+
+      systems = lib.systems.flakeExposed;
+      perSystem = f: lib.genAttrs systems f;
+      perSystem' =
+        f:
+        perSystem (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+
+              overlays = [
+                (pkgs: _: {
+                  scope = lib.makeScope pkgs.newScope (scope: {
+                    inherit self inputs;
+
+                    devShells = {
+                      default = scope.callPackage ./nix/devshells { };
+                    };
+
+                    packages' = {
+                      default = scope.callPackage ./nix/packages { };
+                    };
+
+                    apps = lib.mapAttrs (_: package: {
+                      type = "app";
+                      program = "${lib.getExe package}";
+                    }) scope.packages';
+                  });
+                })
+              ];
+            }
+          )
+        );
+    in
+    {
+      formatter = perSystem' (pkgs: pkgs.nixfmt-tree);
+
+      devShells = perSystem' (pkgs: pkgs.scope.devShells);
+      packages = perSystem' (pkgs: pkgs.scope.packages');
+      apps = perSystem' (pkgs: pkgs.scope.apps);
+    };
+}
---
+++ nix/devshells/default.nix
@@ -0,0 +1,11 @@
+{
+  mkShell,
+  hello,
+  ...
+}:
+
+mkShell {
+  packages = [
+    hello
+  ];
+}
