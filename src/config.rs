47c14b510474fb84a0741b6b31faa0d9b994a03d
threadexio <pzarganitis@gmail.com>
2026-08-31 14:57:30 +0300

    chore: update dependencies etc

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,7 @@
-use std::collections::HashSet;
-use std::fmt;
-use std::fs;
-use std::io;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-use eyre::{Context, Result};
-use module::{Context as _, Merge};
-use module_util::evaluator::Evaluator;
+use eyre::{Result, bail};
+use module::Merge;
 use serde::Deserialize;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -19,73 +14,16 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn read(config_path: &Path) -> Result<Self> {
-        #[derive(Deserialize)]
-        struct Module {
-            #[serde(default)]
-            imports: Vec<PathBuf>,
-
-            #[serde(flatten)]
-            x: Config,
-        }
-
-        #[derive(Clone)]
-        struct DisplayPath(PathBuf);
-
-        impl fmt::Display for DisplayPath {
-            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                self.0.display().fmt(f)
-            }
-        }
-
-        let mut evaluator: Evaluator<DisplayPath, Self> = Evaluator::with(Self::default());
-        let mut evaluated = HashSet::new();
-
-        evaluator.import(DisplayPath(config_path.to_path_buf()));
-
-        while let Some(path) = evaluator.next() {
-            if !evaluated.insert(path.0.clone()) {
-                continue;
-            }
-
-            debug!("evaluating config file '{path}'");
-
-            let text = match fs::read_to_string(&path.0) {
-                Ok(x) => x,
-                Err(e) if e.kind() == io::ErrorKind::NotFound && path.0 == config_path => continue,
-                Err(e) => Err(e).with_context(|| format!("failed to read {}", path))?,
-            };
-
-            let dirname = path
-                .0
-                .parent()
-                .expect("the path must have at least one component since read() succeeded");
-
-            let Module { imports, x } =
-                toml::from_str(&text).with_context(|| format!("failed to parse {path}"))?;
-
-            let imports = imports
-                .into_iter()
-                .map(|x| dirname.join(x))
-                .map(DisplayPath)
-                .collect();
-
-            evaluator
-                .eval(path.clone(), imports, x)
-                .with_trace(|| evaluator.trace(path))?;
-        }
-
-        let x = evaluator
-            .finish()
-            .expect("there must be at least one value");
-
+    pub fn read(path: impl Into<PathBuf>) -> Result<Self> {
+        let path = path.into();
+        let x = module_util::file::toml([path])?.expect("evaluated at least one path");
         Ok(x)
     }
 
     pub fn default_path() -> Result<PathBuf> {
         cfg_select! {
             debug_assertions => {
-                eyre::bail!("there is no default configuration file for debug builds. please specify one manually.")
+                bail!("there is no default configuration file for debug builds. please specify one manually.")
             },
 
             _ => {
